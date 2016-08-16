e94f66b006a92d415933c6a45ff236c09cb48d0d
Marta Sanchez and Thomas Rivas <thomas@>
2016-08-16 09:28:10 -0700

    create a website that counts numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
+  $("form#numbers").submit(function(event){
 
+    var countUntil = parseInt($("#count-until").val());
+    var countBy = parseInt($("#count-by").val());
+
+    var array = [];
+
+    if (countUntil < 0 || countBy < 0) {
+      alert("Please use only positive numbers");
+    } else if (countUntil < countBy){
+      alert ("The number you'll count until should be bigger than the number you'll count by.");
+    } else {
+      for (var i = countBy; i <= countUntil; i += countBy) {
+        array.push(i);
+        var stringResult = array.join(", ");
+        $("#display-count-to").text(countUntil);
+        $("#display-count-by").text(countBy);
+        $("#display-output").text(stringResult);
+        $(".display-result").show();
+      };
+    }
+
+    event.preventDefault();
+
+  });
 });
